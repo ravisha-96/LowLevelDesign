b2a9fb7b9d9efba1d73c718073eb35fbff342a8f
Ravi Kumar <ravikumar@192.168.1.3>
2025-05-09 21:36:36 +0530

    read write lock example

---
+++ multithreading/basics/ReadWriteLockExample.java
@@ -0,0 +1,75 @@
+package multithreading.basics;
+
+import java.sql.Time;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteLockExample {
+    private ReadWriteLock rwLock;
+    private String isAvailable;
+
+    ReadWriteLockExample() {
+        rwLock  = new ReentrantReadWriteLock();
+        isAvailable = "StartingValue";
+    }
+
+    public String readResource(String thread) {
+        try{
+            rwLock.readLock().lock();
+            return isAvailable;
+        }catch(Exception ex){
+
+        }finally {
+            rwLock.readLock().unlock();
+        }
+        return thread;
+    }
+
+    public String writeResource(String thread) {
+        try {
+            rwLock.writeLock().lock();
+            isAvailable = "newValue" + Math.random();
+            return isAvailable;
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+        return isAvailable;
+    }
+
+    public static void main(String[] args) {
+        ReadWriteLockExample rwExample = new ReadWriteLockExample();
+
+        Thread readThread = new Thread(() -> {
+            System.out.println("Reading the value " +rwExample.readResource("readThread"));
+        });
+
+        Thread readThread2 = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Reading the value " +rwExample.readResource("readThread"));
+        });
+
+        Thread writeThread = new Thread(() -> {
+            System.out.println("writing new value" + rwExample.writeResource("writeThread"));
+        });
+
+        Thread writeThread2 = new Thread(() -> {
+            System.out.println("writing new value" + rwExample.writeResource("writeThread"));
+        });
+
+
+        readThread.start();
+        readThread2.start();
+        writeThread.start();
+        writeThread2.start();
+
+        
+
+    }
+}
